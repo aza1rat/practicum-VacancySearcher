f7590a2f05ae44ba7b4416d2b781e97afac0b029
JohnGudron <oak43556@gmail.com>
2026-03-24 19:38:05 +0300

    DI#12

--- app/src/main/java/ru/practicum/android/diploma/util/AuthInterceptor.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/AuthInterceptor.kt
@@ -4,15 +4,15 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor(private val token: String) : Interceptor {
-    companion object {
-        private const val HEADER_AUTHORIZATION = "Authorization"
-        private const val BEARER_PREFIX = "Bearer "
-    }
-
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val builder = originalRequest.newBuilder()
         builder.header(HEADER_AUTHORIZATION, "$BEARER_PREFIX$token")
         return chain.proceed(builder.build())
+    }
+
+    companion object {
+        private const val HEADER_AUTHORIZATION = "Authorization"
+        private const val BEARER_PREFIX = "Bearer "
     }
 }
