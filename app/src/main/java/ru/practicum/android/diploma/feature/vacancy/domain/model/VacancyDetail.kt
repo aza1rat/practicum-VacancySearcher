dca4cf4ca1a628c9f810c0ef2de6e409347f5851
Stanpoor <plusiknoisy98@gmail.com>
2026-03-27 15:04:05 +0300

    поправил VacancyDetail, VacancyDetailMapper

--- app/src/main/java/ru/practicum/android/diploma/feature/vacancy/domain/model/VacancyDetail.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/vacancy/domain/model/VacancyDetail.kt
@@ -1,28 +1,78 @@
 package ru.practicum.android.diploma.feature.vacancy.domain.model
 
-import ru.practicum.android.diploma.feature.search.data.dto.AddressDto
-import ru.practicum.android.diploma.feature.search.data.dto.AreaDto
-import ru.practicum.android.diploma.feature.search.data.dto.ContactsDto
-import ru.practicum.android.diploma.feature.search.data.dto.EmployerDto
-import ru.practicum.android.diploma.feature.search.data.dto.EmploymentDto
-import ru.practicum.android.diploma.feature.search.data.dto.ExperienceDto
-import ru.practicum.android.diploma.feature.search.data.dto.IndustryDto
-import ru.practicum.android.diploma.feature.search.data.dto.SalaryDto
-import ru.practicum.android.diploma.feature.search.data.dto.ScheduleDto
-
 data class VacancyDetail(
     val id: String,
     val name: String,
-    val salary: SalaryDto? = null,
-    val address: AddressDto? = null,
-    val experience: ExperienceDto? = null,
-    val schedule: ScheduleDto? = null,
-    val employment: EmploymentDto? = null,
-    val contacts: ContactsDto? = null,
+    val salary: Salary? = null,
+    val address: Address? = null,
+    val experience: Experience? = null,
+    val schedule: Schedule? = null,
+    val employment: Employment? = null,
+    val contacts: Contacts? = null,
     val description: String? = null,
-    val employer: EmployerDto? = null,
-    val area: AreaDto? = null,
+    val employer: Employer? = null,
+    val area: Area? = null,
     val skills: List<String>? = null,
     val url: String? = null,
-    val industry: IndustryDto? = null
+    val industry: Industry? = null
+)
+
+data class Salary(
+    val id: String? = null,
+    val currency: String? = null,
+    val from: Int? = null,
+    val to: Int? = null
+)
+
+data class Address(
+    val id: String? = null,
+    val city: String? = null,
+    val street: String? = null,
+    val building: String? = null,
+    val raw: String? = null
+)
+
+data class Experience(
+    val id: String? = null,
+    val name: String? = null
+)
+
+data class Schedule(
+    val id: String? = null,
+    val name: String? = null
+)
+
+data class Employment(
+    val id: String? = null,
+    val name: String? = null
+)
+
+data class Contacts(
+    val id: String? = null,
+    val name: String? = null,
+    val email: String? = null,
+    val phones: List<Phone>? = null
+)
+
+data class Phone(
+    val comment: String? = null,
+    val formatted: String? = null
+)
+
+data class Employer(
+    val id: String? = null,
+    val name: String? = null,
+    val logo: String? = null
+)
+
+data class Area(
+    val id: String? = null,
+    val parentId: String? = null,
+    val name: String? = null,
+    val areas: List<Area>? = null
+)
+
+data class Industry(
+    val id: String? = null,
+    val name: String? = null
 )
